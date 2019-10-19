e6467867b4dbdee2750b461a3e28fdb924c0ebc1
David Lodi <lodi.dev@outlook.com>
2019-10-19 18:03:56 -0500

    comenzado a trabajar en la lista

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomePagesTemp extends StatelessWidget {
+  final opciones = ['uno', 'dos', 'tres', 'cuaro'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        /*   children: _crearItems(), */
+        children: _crearItemsCorta(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      //operador de cascada
+      lista..add(tempWidget)..add(Divider());
+    }
+    return lista;
+  }
+
+//Metodo corto regresa una nueva lista
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!!'),
+            subtitle: Text('Arreglo'),
+            leading: Icon(Icons.adb),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: (){},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+}
