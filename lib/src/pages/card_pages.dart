49da00bc01e9ccd3e3150512d93831ed33075f93
David Lodi <lodi.dev@outlook.com>
2019-10-20 16:49:42 -0500

    add loading to insert images

--- lib/src/pages/card_pages.dart
+++ lib/src/pages/card_pages.dart
@@ -9,7 +9,11 @@ class CardPages extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: <Widget>[_crearTipo1()],
+        children: <Widget>[
+          _crearTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+        ],
       ),
     );
   }
@@ -21,21 +25,42 @@ class CardPages extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.photo_album, color: Colors.purple),
             title: Text('Soy El titulo'),
-            subtitle: Text('Un subtitulo para una app de flutter que cambiara mi vida como desarrollador'),
+            subtitle: Text(
+                'Un subtitulo para una app de flutter que cambiara mi vida como desarrollador'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               FlatButton(
                 child: Text('Cancelar'),
-                onPressed: (){},
+                onPressed: () {},
               ),
               FlatButton(
                 child: Text('Ok'),
-                onPressed: (){},
+                color: Colors.teal,
+                onPressed: () {},
               )
-            ]
-            ,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+
+          FadeInImage(
+            image:  NetworkImage('https://wallpaperplay.com/walls/full/5/4/f/34814.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          ),
+
+        Container(
+          padding: EdgeInsets.all(10.0),
+          child:   Text('I love 80s'),
+          )
         ],
       ),
     );
