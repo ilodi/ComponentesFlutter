37db7abcb4a987b6cde6b4bb1d5626ce794ce18f
David Lodi <lodi.dev@outlook.com>
2019-10-19 21:51:21 -0500

    se puede leer archivos json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../providers/menu_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -7,23 +8,38 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Componentes!'),
         elevation: 1.0,
-
       ),
       body: _lista(),
     );
   }
 
   Widget _lista() {
-    return ListView(
-      children: _listaItems(),
+    //menuProvider.cargarData()
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget>_listaItems() {
-    return[
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.aspect_ratio, color: Colors.blue,),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue,),
+        onTap: (){
 
+        },
+      );
+       opciones..add( widgetTemp )
+               ..add(Divider());
+    });
+    return opciones;
   }
 }
