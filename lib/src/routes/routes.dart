21b4d8624e5bfefeeeab9d5e1ad32603f821b61f
David Lodi <lodi.dev@outlook.com>
2019-10-20 22:21:23 -0500

    Aprendiendo inputs

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import '../pages/alert_page.dart';
 import '../pages/avatar_page.dart';
 import '../pages/card_pages.dart';
 import '../pages/animated_container.dart'; 
+import '../pages/input_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
@@ -13,5 +14,6 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'avatar'    : (BuildContext context) => AvatarPage(),
         'card'      : (BuildContext context) => CardPages(),
         'animatedContainer'  : (BuildContext context) => AnimatedContainerPages(),
+        'inputs'             : (BuildContext context) => InputPage(),
       };
 }
